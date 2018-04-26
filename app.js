67672de1ffd640672bd659c18389276585686c96
Ian Avery <ianedavery@gmail.com>
2018-04-26 11:22:29 -0700

    added runServer and closeServer functions to app.js

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-var express = require('express');
-var logger = require('morgan');
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const {DATABASE_URL, PORT} = require('./config');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+//allow mongoose to use ES6 promises
+mongoose.Promise = global.Promise;
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -12,4 +17,43 @@ app.use(express.json());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-module.exports = app;
+let server;
+
+const runServer = (databaseUrl = DATABASE_URL, port = PORT) => {
+	return new Promise((resolve, reject) => {
+		mongoose.connect(databaseUrl, err => {
+			server = app
+				.listen(port, () => {
+					console.log(`Your app is listening on port ${PORT}`);
+					resolve();
+				})
+				.on('error', err => {
+					mongoose.disconnect();
+					reject(err)
+				});
+		});
+	});
+}
+
+const closeServer = () => {
+	return mongoose.disconnect()
+		.then(() => {
+			return new Promise((resolve, reject) => {
+				console.log('closing server');
+				server.close(err => {
+					if(err) {
+						return reject(err);
+					}
+					resolve();
+				});
+			});
+		});
+}
+
+//this is a useful trick for making this file both an executable script, and a module.
+//this will be helpful when we are running tests.
+if(require.main === module) {
+	runServer().catch(err => console.log(err));
+};
+
+module.exports = {app, runServer, closeServer};
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
-/* GET home page. */
 router.get('/', (req, res) => {
   res.send('hello');
 });
